6f2aa208d2925d5c9eb8a91abbf985a919205349
GregerGundersen <gregergundersen@gmail.com>
2021-05-01 18:51:27 +0200

    Footer

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
-const url = "https://dionysus.no/projectexam/wp-json/wp/v2/posts";
+const mediaUrl =
+  "https://dionysus.no/projectexam/wp-json/wp/v2/posts/?_embed=wp:featuredmedia";
 const recipesOut = document.querySelector("div.recipes");
 
-fetch(url)
+/* Fetch post and media */
+fetch(mediaUrl)
   .then((response) => response.json())
-  .then((data) => {
-    console.log("Success:", data);
-    displayRecipes(data);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-    recipesOut.innerHTML = `
-    <div id="error">
-    <h2 >Error:</h2>
-    <p>${error}</p>
-    </div>`;
-  });
+  .then((data) => displayMedia(data))
+  .catch((error) => console.error("Error: " + error));
 
-const displayRecipes = (recipes) => {
-  let recipeList = "";
+function displayMedia(recipes) {
   for (let recipe of recipes) {
-    // console.log(product);
-    recipeList += `
-        <a href="https://projectexam.netlify.app/recipe.html?id=${recipe.id}" class="recipeblock">
-          <h2>${recipe.title.rendered}</h2>
-          ${recipe.excerpt.rendered}
-        </a>
-        `;
+    console.log(recipe);
+    let imgSrc =
+      recipe._embedded["wp:featuredmedia"][0].media_details.sizes.medium
+        .source_url;
+    let altText = recipe._embedded["wp:featuredmedia"][0].alt_text;
+    recipesOut.innerHTML += `
+                            <div class="recipeblock">
+                              <div class="recipeimagecontainer">
+                                <img src="${imgSrc}" alt="${altText}"></img>
+                              </div>
+                              <h2>${recipe.title.rendered}</h2>
+
+                            </div>`;
   }
-  recipesOut.innerHTML = recipeList;
-};
+}
+/* style = "background-image:url('${imgSrc}')"; */
