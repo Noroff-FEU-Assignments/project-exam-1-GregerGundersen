e1a8a781951faff88aaef159426929a734990886
GregerGundersen <gregergundersen@gmail.com>
2021-05-04 11:56:21 +0200

    Recipe Hover

--- js/script.js
+++ js/script.js
@@ -10,22 +10,30 @@ fetch(mediaUrl)
 
 function displayMedia(recipes) {
   for (let recipe of recipes) {
-    console.log(recipe);
+    // console.log(recipe);
     let id = recipe.id;
     let imgSrc =
       recipe._embedded["wp:featuredmedia"][0].media_details.sizes.medium
         .source_url;
     let altText = recipe._embedded["wp:featuredmedia"][0].alt_text;
     recipesOut.innerHTML += `
-                            <a href="https://projectexam.netlify.app/recipe.html/?id=${id}"
+                            <a class="recipelink" href="https://projectexam.netlify.app/recipe.html/?id=${id}"
                             <div class="recipeblock">
                               <div class="recipeimagecontainer">
                                 <img src="${imgSrc}" alt="${altText}"></img>
                               </div>
-                              <h2>${recipe.title.rendered}</h2>
+                              <h2 class="recipeheader">${recipe.title.rendered}</h2>
 
                             </div>
                             </a>`;
   }
 }
-/* style = "background-image:url('${imgSrc}')"; */
+
+/* Recipe Styling */
+// const recipeLink = document.querySelector(".recipelink");
+// console.log(recipeLink);
+// const recipeHeader = document.querySelector(".recipeheader");
+// const headerColor = () => {
+//   recipeHeader.style.color = "var(--maincolor)";
+// };
+// recipeLink.addEventListener("mouseover", headerColor);
